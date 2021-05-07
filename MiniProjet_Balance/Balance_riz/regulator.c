0b97ef3b7c6fd357e96603442a3aacba3f29fff6
bloues42 <31881160+bloues42@users.noreply.github.com>
2021-05-07 12:07:45 +0200

    ça marche
    - il se redresse
    - il bouge dans la bonne direction

--- MiniProjet_Balance/Balance_riz/regulator.c
+++ MiniProjet_Balance/Balance_riz/regulator.c
@@ -23,37 +23,74 @@ int16_t regulator(float error){
 }
 
 int16_t rotation_regulator(int16_t speed){
-	static uint16_t last_rot_error = 0;
+	static uint16_t last_fr_right = 0;
+	static uint16_t last_fr_left = 0;
 	float speed_correction = 0;
 	uint16_t rot_error;
-	int32_t ir_left = 0, ir_right = 0;
+	int32_t fr_left = 0, fr_right = 0, left = 0, right = 0;
 
-	ir_left = get_calibrated_prox(IR_LEFT);
-	ir_right = get_calibrated_prox(IR_RIGHT);
+	//ir_left = get_calibrated_prox(IR_LEFT);
+	//ir_right = get_calibrated_prox(IR_RIGHT);
+
+	left = get_calibrated_prox(IR_LEFT);
+	right = get_calibrated_prox(IR_RIGHT);
+
+	fr_left = get_calibrated_prox(IR_FRONT_LEFT45);
+	fr_right = get_calibrated_prox(IR_FRONT_RIGHT45);
 
 	if(speed > 0){
 		//computes a correction factor to let the robot rotate in the middle of the platform
-		if(ir_left>ROTATION_THRESHOLD){
-			rot_error = ir_left;
+		if(fr_left > fr_right){
+			speed_correction = ROTATION_COEFF * (fr_left-last_fr_left);
 		}
-		if(ir_right>ROTATION_THRESHOLD){
-			rot_error = - ir_right;
+
+		if(fr_right > fr_left){
+			speed_correction = - ROTATION_COEFF * (fr_right-last_fr_right);
 		}
-	}else if(speed < 0){
-		//computes a correction factor to let the robot rotate in the middle of the platform
-		if(ir_left>ROTATION_THRESHOLD){
-			rot_error = - ir_left;
+
+		last_fr_left = fr_left;
+		last_fr_right = fr_right;
+
+		if((left > SIDE_STOP) && (speed_correction < 0)){
+			speed_correction = 0;
 		}
-		if(ir_right>ROTATION_THRESHOLD){
-			rot_error = ir_right;
+
+		if((right > SIDE_STOP) && (speed_correction > 0)){
+			speed_correction = 0;
 		}
-	}else{
-		rot_error = 0;
+
 	}
 
-	speed_correction = ROTATION_COEFF * (rot_error-last_rot_error);
 
-	last_rot_error = rot_error;
+	else if(speed < 0){
+		//computes a correction factor to let the robot rotate in the middle of the platform
+		if(fr_left > fr_right){
+			speed_correction = - ROTATION_COEFF * (fr_left-last_fr_left);
+		}
+
+		if(fr_right > fr_left){
+			speed_correction = ROTATION_COEFF * (fr_right-last_fr_right);
+		}
+
+		last_fr_left = fr_left;
+		last_fr_right = fr_right;
+
+		if((left > SIDE_STOP) && (speed_correction > 0)){
+			speed_correction = 0;
+		}
+
+		if((right > SIDE_STOP) && (speed_correction < 0)){
+			speed_correction = 0;
+		}
+	}else{
+		speed_correction = 0;
+	}
+
+	//speed_correction = ROTATION_COEFF * (rot_error-last_rot_error);
+
+	//last_rot_error = rot_error;
+
+	chprintf((BaseSequentialStream *)&SD3, "frontleft=%d   frontright=%d    correction=%f \r\n", fr_left, fr_right, speed_correction);
     return (int16_t)speed_correction;
 }
 
@@ -66,9 +103,9 @@ static THD_FUNCTION(Regulator, arg) {
     systime_t time;
     static int16_t speed = 0;
     int16_t speed_correction = 0;
-    static int j = 0;
+    static uint16_t j = 0, k = 0;
     int32_t ir_back;
-    float mean; //A SUPPRIMER
+
 
     while(1){
         time = chVTGetSystemTime();
@@ -79,15 +116,34 @@ static THD_FUNCTION(Regulator, arg) {
 
 			//computes the speed to give to the motors
 			//the value of the error is obtained from the mean of the collected samples
-			mean = get_mean_error(); //A SUPPRIMER
-        	speed = regulator(mean);
+        	speed = regulator(get_mean_error());
+        	speed_correction = rotation_regulator(speed);
         }
 
-    	//compute speed correction to straighten the robot
-    	speed_correction = rotation_regulator(speed);
+/* ALLER_RETOUR
+        if(j<=300){
+			speed = -1;
+			j++;
+		}
+		else if(j>300){
+			speed = 1;
+			j++;
+		}
+        if(j==600){
+        	j=0;
+        }
 
+        if(k==10){
+    	//compute speed correction to straighten the robot
+        	speed_correction = rotation_regulator(speed);
+    		//chprintf((BaseSequentialStream *)&SD3, "frontleft=%d   frontright=%d    correction=%d \r\n", get_calibrated_prox(IR_FRONT_LEFT45), get_calibrated_prox(IR_FRONT_RIGHT45), speed_correction);
+    		k=0;
+    	}
+    	 k++;
+*/
 		//chprintf((BaseSequentialStream *)&SD3, "mean_err=%f speed=%d \r\n", mean, speed);
-		//chprintf((BaseSequentialStream *)&SD3, "corr=%d LEFT=%d RIGHT=%d \r\n", speed_correction, ir_left, ir_right);
+		//chprintf((BaseSequentialStream *)&SD3, "frontleft=%d   frontright=%d    LEFT=%d   RIGHT=%d \r\n", get_calibrated_prox(IR_FRONT_LEFT45), get_calibrated_prox(IR_FRONT_RIGHT45), get_calibrated_prox(IR_LEFT),get_calibrated_prox(IR_RIGHT));
+
 
 
 		//checks if the robot is too close to the back wall
@@ -103,6 +159,11 @@ static THD_FUNCTION(Regulator, arg) {
 		//applies the speed from the regulator and the correction for the rotation
 		right_motor_set_speed(speed - speed_correction);
 		left_motor_set_speed(speed + speed_correction);
+
+
+
+		//right_motor_set_speed(500*speed - speed_correction);
+		//left_motor_set_speed(500*speed + speed_correction);
 
         //100Hz
         chThdSleepUntilWindowed(time, time + MS2ST(10));
